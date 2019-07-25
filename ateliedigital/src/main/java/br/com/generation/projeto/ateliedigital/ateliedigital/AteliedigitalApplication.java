5a1fbdd6a80c095801758bf4aacd7826c01f1771
pabloBarrantesPolan <pablobp.cc@gmail.com>
2019-07-25 12:49:24 -0300

    versao cliente back ok

---
+++ ateliedigital/src/main/java/br/com/generation/projeto/ateliedigital/ateliedigital/AteliedigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.projeto.ateliedigital.ateliedigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteliedigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteliedigitalApplication.class, args);
+	}
+
+}
