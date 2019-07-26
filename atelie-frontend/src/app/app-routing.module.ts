b980ff887e5e1fb3137a992610500a557f71598e
pabloBarrantesPolan <pablobp.cc@gmail.com>
2019-07-26 10:55:33 -0300

    fix Cors erro

--- atelie-frontend/src/app/app-routing.module.ts
+++ atelie-frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ClienteListComponent} from './cliente/cliente-list/cliente-list.component';
@@ -7,7 +8,7 @@ import { ClienteDetailComponent} from './cliente/cliente-detail/cliente-detail.c
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'clientes', pathMatch: 'full'},
+  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
   {path: 'clientes', component: ClienteListComponent},
   {path: 'adicionar', component: ClienteCreateComponent},
   {path: 'detalhe/:id', component: ClienteDetailComponent}
--- ateliedigital/src/main/java/br/com/generation/projeto/ateliedigital/ateliedigital/cliente/ClienteController.java
+++ ateliedigital/src/main/java/br/com/generation/projeto/ateliedigital/ateliedigital/cliente/ClienteController.java
@@ -16,6 +16,7 @@ import java.util.Optional;
 @RestController
 @Api
 @RequestMapping(value = "/api/v1/clientes", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@CrossOrigin("*")
 public class ClienteController {
 
     private static final Logger logger = LoggerFactory.getLogger(ClienteController.class);
