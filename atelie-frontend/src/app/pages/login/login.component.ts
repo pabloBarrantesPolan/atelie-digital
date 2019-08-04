b876fbe8e4596434c3d62f3b9197e63ab5743f32
unknown <diego.souza0403@gmail.com>
2019-08-04 16:52:59 -0300

    login autenticado

--- atelie-frontend/src/app/pages/login/login.component.ts
+++ atelie-frontend/src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
 import { Router } from  '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
+import { Profissional } from 'src/app/profissional/profissional';
 
 
 @Component({
@@ -11,29 +12,15 @@ import { AuthService } from 'src/app/auth/auth.service';
 })
 export class LoginComponent {
 
-  loginForm: FormGroup;
-  isSubmitted  =  false;
-
+private profissional: Profissional = new Profissional;
   constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
 
-    this.loginForm  =  this.formBuilder.group({
-      email: ['', Validators.required],
-      senha: ['', Validators.required]
-  });
-
 }
 
 login(){
-  console.log(this.loginForm.value);
-  this.isSubmitted = true;
-  if(this.loginForm.invalid){
-    return;
+  this.authService.login(this.profissional)
   }
-  this.authService.login(this.loginForm.value);
-  this.router.navigateByUrl('/home');
 }
 
-get formControls() { return this.loginForm.controls; }
-}
